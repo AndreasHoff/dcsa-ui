293ad79622921d590a545a01482b22f57d8fcbe2
Andreas <ah@asseco.dk>
2021-08-31 13:46:42 +0200

    scrollable form

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -7,24 +7,95 @@ import { IDropdownSettings } from 'ng-multiselect-dropdown';
   styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
-  dropdownList = <any>[];
-  selectedItems = <any>[];
-  dropdownSettings: IDropdownSettings;
+  dropdownListEventType = <any>[];
+  selectedItemsEventType = <any>[];
+  dropdownSettingsEventType: IDropdownSettings;
+
+  dropdownListTransportType = <any>[];
+  selectedItemsTransportType = <any>[];
+  dropdownSettingsTransportType: IDropdownSettings;
+
+  dropdownListOperations = <any>[];
+  selectedItemsOperations = <any>[];
+  dropdownSettingsOperations: IDropdownSettings;
 
   ngOnInit() {
-    this.dropdownList = [
+    // Event type multiselect
+    this.dropdownListEventType = [
+      { item_id: 1, item_text: 'Transport' },
+      { item_id: 2, item_text: 'Operations' },
+    ];
+    this.selectedItemsEventType = [
+      { item_id: 1, item_text: 'Transport' },
+      { item_id: 2, item_text: 'Operations' },
+    ];
+    this.dropdownSettingsEventType = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      /* itemsShowLimit: 3, */
+      /* allowSearchFilter: true, */
+    };
+
+    //Transport Event type multiselect
+    this.dropdownListTransportType = [
+      { item_id: 1, item_text: 'Arrived' },
+      { item_id: 2, item_text: 'Departed' },
+    ];
+    this.selectedItemsTransportType = [
+      { item_id: 1, item_text: 'Arrived' },
+      { item_id: 2, item_text: 'Departed' },
+    ];
+    this.dropdownSettingsTransportType = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      /* itemsShowLimit: 3, */
+      /* allowSearchFilter: true, */
+    };
+
+    // Operations event multiselect
+    this.dropdownListOperations = [
+      { item_id: 1, item_text: 'Started' },
+      { item_id: 2, item_text: 'Completed' },
+      { item_id: 3, item_text: 'Arrived' },
+      { item_id: 4, item_text: 'Departed' },
+      { item_id: 1, item_text: 'Started' },
+      { item_id: 2, item_text: 'Completed' },
+      { item_id: 3, item_text: 'Arrived' },
+      { item_id: 4, item_text: 'Departed' },
+      { item_id: 1, item_text: 'Started' },
+      { item_id: 2, item_text: 'Completed' },
+      { item_id: 3, item_text: 'Arrived' },
+      { item_id: 4, item_text: 'Departed' },
+      { item_id: 1, item_text: 'Started' },
+      { item_id: 2, item_text: 'Completed' },
+      { item_id: 3, item_text: 'Arrived' },
+      { item_id: 4, item_text: 'Departed' },
+      { item_id: 1, item_text: 'Started' },
+      { item_id: 2, item_text: 'Completed' },
+      { item_id: 3, item_text: 'Arrived' },
+      { item_id: 4, item_text: 'Departed' },
+      { item_id: 1, item_text: 'Started' },
+      { item_id: 2, item_text: 'Completed' },
+      { item_id: 3, item_text: 'Arrived' },
+      { item_id: 4, item_text: 'Departed' },
       { item_id: 1, item_text: 'Started' },
       { item_id: 2, item_text: 'Completed' },
       { item_id: 3, item_text: 'Arrived' },
       { item_id: 4, item_text: 'Departed' },
     ];
-    this.selectedItems = [
+    this.selectedItemsOperations = [
       { item_id: 1, item_text: 'Started' },
       { item_id: 2, item_text: 'Completed' },
       { item_id: 3, item_text: 'Arrived' },
       { item_id: 4, item_text: 'Departed' },
     ];
-    this.dropdownSettings = {
+    this.dropdownSettingsOperations = {
       singleSelection: false,
       idField: 'item_id',
       textField: 'item_text',
